547d908db30ae8a9e5c37fa1337479affa60d09c
coalsticks <blazergirl.simplyme@gmail.com>
2023-04-19 00:05:36 -0300

    update

--- app.py
+++ app.py
@@ -6,14 +6,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-
+    # response = request.get("")
 
     return render_template("home.html")
 
 
 @app.route('/location-weather', methods=['GET', 'POST'])
 def location_weather():
-    return render_template("location.html", location='brazil')
+    location = str(request.form.get("search").replace(" ", "+"))
+    response = requests.get("https://nominatim.openstreetmap.org/search?q={location}&format=json&limit=1")
+    lat = response.json()[0]['lat']
+    lon = response.json()[0]['lon']
+
+    reverse_test = requests.get("https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format=jsonv2")
+
+    return render_template("location.html", location=reverse_test, lat=lat, lon=lon)
 
 
 
